71bbf320ef46e97ae504d018bbf94ed0727aa08a
tanbirk <tanvir.kashyap879@gmail.com>
2021-11-30 15:03:50 +0530

    gang

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,14 +1,166 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, View} from 'react-native'
+import { useLayoutEffect } from 'react'
+import { Avatar } from 'react-native-elements'
+import { TouchableOpacity } from 'react-native'
+import { AntDesign, FontAwesome, Ionicons } from "@expo/vector-icons"
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar'
+import { ScrollView, TextInput, TouchableWithoutFeedback } from 'react-native-gesture-handler'
+import { db, auth } from '../firebase'
+import * as firebase from "firebase"
 
-const ChatScreen = () => {
+const ChatScreen = ({ navigation, route }) => {
+    const [input, setInput] = useState('')
+    const [messages, setMessages] =useState([])
+
+    useLayoutEffect(() => {
+       navigation.setOptions({
+            title:"Chat",
+            headerBackTitleVisible: false,
+            headerTitleAlign:"left",
+            headerTitle: () => (
+                <View
+                style={{
+                    flexDirection:'row',
+                    alignItems: 'center'
+                }}
+                >
+                    <Avatar 
+                    rounded
+                    source={{
+                        uri:"https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
+                    }}
+                    />
+                    <Text style={{color: 'white', marginLeft: 10, fontWeight: '700'}}>
+                        {route.params.chatName}
+                    </Text>
+                </View>
+            ),
+            headerLeft:() => (
+                <TouchableOpacity
+                style={{ marginLeft: 10}}
+                onPress={navigation.goBack}
+                >
+                        <AntDesign
+                        name='arrowleft'
+                        size={24}
+                        color="white" />
+                </TouchableOpacity>
+            ),
+            headerRight: () => (
+                <View
+                   style={{
+                       flexDirection: 'row',
+                       justifyContent: 'space-between',
+                       width: 80,
+                       marginRight: 20,
+                   }}
+                
+                >
+                    <TouchableOpacity>
+                        <FontAwesome name="video-camera" size={24} color='white' />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Ionicons name='call' size={24} color='white' />
+                    </TouchableOpacity>
+                    
+                </View>
+            )
+       }) 
+       
+    }, [navigation])
+
+    const sendMessage =() =>{
+            Keyboard.dismiss();
+
+
+            db.collection('chats').doc(route.params.id)
+            .collection('messages').add({
+                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                message: input,
+                displayName: auth.currentUser.displayName,
+                email: auth.currentUser.email,
+                photoURL: auth.currentUser.photoURL
+            })
+            setInput('')
+    }
+
+    useLayoutEffect(() => {
+        const unsubscribe =db.collection('chats')
+        .doc(route.params.id)
+        .collection('messages')
+        .orderBy('timestamp', 'desc')
+        .onSnapshot(snapshot => setMessages(
+            snapshot.docs.map(doc => ({
+                id: doc.id,
+                data: doc.data()
+            }))
+        ))
+        return unsubscribe;
+    }, [route])
+
     return (
-        <View>
-            <Text>Chat Screen</Text>
-        </View>
+        <SafeAreaView style={{ flex:1, backgroundColor: 'white'}}>
+           <StatusBar style='light' />
+            <KeyboardAvoidingView 
+            behavior={Platform.OS === 'ios' ? "padding" : "height"}
+            style={styles.container}
+            keyboardVerticalOffset={90} >
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss} />
+                    
+                    <ScrollView>
+                    {messages.map(({id, data}) => (
+                        data.email === auth.currentUser.email ? (
+                            <View></View>
+                        ) : (
+                            <View></View>
+                        )
+                    ))}
+
+                    </ScrollView>
+                    <View style={styles.footer}>
+                        <TextInput
+                        value={input}
+                        onChangeText={(text) => setInput(text)}  
+                        onSubmitEditing={sendMessage}  
+                        placeholder="Signal Message"
+                        style={styles.textInput}
+                        />
+                        <TouchableOpacity
+                        onPress={sendMessage}
+                        activeOpacity={0.5} >
+                            <Ionicons name="send" size={24} color='#2b68e6' />
+                        </TouchableOpacity>
+                    </View>
+               
+                </KeyboardAvoidingView>
+        </SafeAreaView>
     )
 }
 
 export default ChatScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+            flex: 1,
+    }, 
+    footer:{
+            flexDirection: 'row',
+            alignItems:'center',
+            width: '100%',
+            padding: 15,
+    },
+    textInput:{
+            bottom: 0,
+            height: 40,
+            flex: 1,
+            marginRight: 15,
+            //  borderColor: 'transparent',
+            backgroundColor: '#ececec',
+            //  borderWidth: 1,
+            padding: 10,
+            color: 'grey',
+            borderRadius: 30
+    }
+})
